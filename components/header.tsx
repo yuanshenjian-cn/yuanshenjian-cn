68decee2f0febe4b963be1f435dbb022749f4727
Yuan Shenjian <yuanshenjian@foxmail.com>
2026-04-16 21:51:29 +0800

    polish tab
    Signed-off-by: Yuan Shenjian <yuanshenjian@foxmail.com>

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,8 @@ import { HeaderClient } from "./header-client";
 import { getPostsForSearch } from "@/lib/blog";
 
 const navItems = [
-  { href: "/", label: "首页" },
-  { href: "/articles", label: "文章" },
   { href: "/ai", label: "AI", icon: "sparkles" },
+  { href: "/articles", label: "文章" },
   { href: "/resume", label: "简历" },
 ];
 
